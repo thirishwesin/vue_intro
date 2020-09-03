25596f62dbd5d2d0928f53ea5c86ed5881f513d9
thirishwesin <211phoothiri@gmail.com>
2020-09-03 14:42:31 +0630

    vue testing

---
+++ cli/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    
+    <Testing msg="Welcome to My Vue.js app"/>
+  </div>
+</template>
+
+<script>
+import Testing from './components/Testing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    
+    Testing
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ cli/src/components/Testing.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+	<h1>{{title}}</h1>
+	<p>{{msg}}</p>
+	<p>This is testing for child component</p>
+</div>
+</template>
+
+<script type="text/javascript">
+	export default{
+		props:{
+		msg:null
+		},
+		data (){
+			return {
+				title:'Testing vue'
+			}
+		}
+	}
+</script>
+<style type="text/css">
+	
+</style>
